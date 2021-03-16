9013d0aa49d4a9c425c9ed6935c99c74de7b5f1a
Dylan Cox <19005775@vcconnect.co.za>
2021-03-16 15:17:54 +0200

    Amending

---
+++ AccessControlSystem/Controllers/VisitorController.cs
@@ -0,0 +1,39 @@
+﻿using AccessControlSystem.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessControlSystem.Controllers
+{
+    public class VisitorController : Controller
+    {
+        AccessControl _context;
+        IHostEnvironment webHostEnvironment;
+
+        public VisitorController(AccessControl context, IHostEnvironment webHost)
+        {
+            _context = context;
+            webHostEnvironment = webHost;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Employees);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string name, string reason, string whom, List<IFormFile> file)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+   
+        
+    }
+}
---
+++ AccessControlSystem/Models/ImageModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessControlSystem.Models
+{
+    public class ImageModel
+    {
+        public IFormFile ProfileImage { get; set; }
+    }
+}
